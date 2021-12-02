015de0cbb653b52b4b4371bfcb6c1ab28969ffc2
Jessica Jesus <jessicafj7@gmail.com>
2021-12-02 17:22:38 +0000

    started header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import { Fragment, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../../components/style.scss";
 import {
   AppBar,
   Button,
@@ -15,67 +16,11 @@ import { makeStyles } from "@mui/styles";
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
 
-const useStyles = makeStyles((theme) => ({
-  row: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  container: {},
-  buttonFontSize: {
-    fontSize: "11px",
-    color: "#a1a1a1",
-  },
 
-  AppBar: {
-    colorPrimary: "#00A3A3",
-    backgroundSize: "cover",
-  },
-
-  logo: {
-    maxWidth: 160,
-    color: "#a1a1a1",
-    justifyContent: "left",
-    "&:hover": {
-      background: "transparent",
-    },
-  },
-
-  typo: {
-    marginRight: "10px",
-    fontSize: "11px",
-    color: "#a1a1a1",
-  },
-
-  loginButton: {
-    background: "#89eded",
-    marginRight: "10px",
-    color: "#fff",
-    borderRadius: "25px",
-    padding: "0px 25px",
-
-    "&:hover": {
-      background: "#0b6275",
-      boxShadow: "0px 2px 10px #888888",
-    },
-  },
-  registerButton: {
-    background: "#0b6275",
-    color: "#fff",
-    borderRadius: "25px",
-    padding: "0px 25px",
-
-    "&:hover": {
-      background: "#89eded",
-      boxShadow: "0px 2px 10px #888888",
-    },
-  },
-}));
 
 const Header = () => {
-  const [search,setSearch] = useState("");
-  const [visible,setVisible] = useState(false);
+  const [search, setSearch] = useState("");
+  const [visible, setVisible] = useState(false);
   const isLoggedIn = false;
   const history = useNavigate();
 
@@ -100,115 +45,43 @@ const Header = () => {
   };
 
 
-  const classes = useStyles();
 
   return (
-    <Fragment>
-      <AppBar position="static" color="primary" className={classes.AppBar}>
-        <Grid item sm={12} xs={12} className={classes.container}>
-          <Toolbar>
-            <Grid className={classes.grow}>
-             <img src="amuseBranco.png" alt="logo" className={classes.logo} />
-            </Grid>
-            {isLoggedIn && (
-              <Fragment>
-                {visible &&
-                <TextField
-                  variant="outlined"
-                  size="small"
-                  id="outlined-size-small"
-                  type="text"
-                  label="Search User"
-                  name="name"
-                  value={search}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <Link2
-                  onClick={()=>{
-                    history("/profile/" + search);
-                    setVisible(false);
-                    setSearch("");
-                  }}
-                  variant="body2"
-                >
-                  <SearchIcon />
-                </Link2>
-                <Link2
-                  onClick={() => {
-                    setVisible(false);
-                    setSearch("");
-                  }}
-                  variant="body2"
-                >
-                  <CloseIcon />
-                </Link2>
-                      </InputAdornment>
-                    ),
-                  }}
-                  onChange={(event) => {
-                    setSearch(event.target.value);
-                  }}
-                  autoComplete="name"
-                />}
-                {!visible && <Grid item>
-                <Link2
-                  onClick={() => {
-                    setVisible(true);
-                  }}
-                  variant="body2"
-                >
-                  <SearchIcon />
-                </Link2>
-              </Grid>}
-                <Button
-                  color="inherit"
-                  className={classes.buttonFontSize}
-                  onClick={exploreHandler}
-                >
-                  Discover
-                </Button>
-                <Button
-                  color="inherit"
-                  className={classes.buttonFontSize}
-                  onClick={profileHandler}
-                >
-                  Profile
-                </Button>
-                <Typography className={classes.typo}>
-                  Logged in as
-                </Typography>
-                <Button
-                  color="inherit"
-                  className={classes.buttonFontSize && classes.loginButton}
-                  onClick={logoutHandler}
-                >
-                  Logout
-                </Button>
-              </Fragment>
-            )}
-            {!isLoggedIn && (
-              <Fragment>
-                <Button
-                  color="inherit"
-                  className={classes.buttonFontSize && classes.loginButton}
-                  onClick={loginHandler}
-                >
-                  Activities
-                </Button>
-                <Button
-                  color="inherit"
-                  className={classes.buttonFontSize && classes.registerButton}
-                  onClick={registerHandler}
-                >
-                  Museums
-                </Button>
-              </Fragment>
-            )}
-          </Toolbar>
-        </Grid>
-      </AppBar>
-    </Fragment>
+    <nav className="navbar navbar-expand-lg navbar-blueMuseum bg-blueMuseum">
+  <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="#">Action</a>
+          <a className="dropdown-item" href="#">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
   );
 };
 
