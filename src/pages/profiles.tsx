b65482d6c4ca7ed699df98c4a3bc550036774adb
1000roy <a.milroy@campus.fct.unl.pt>
2021-12-05 01:22:56 +0000

    perfil

--- src/pages/profiles.tsx
+++ src/pages/profiles.tsx
@@ -1,11 +1,14 @@
 import React, { Fragment } from 'react';
 import ActivitiesJson from "../json/ActivitiesJson.json"
-import Profile from "../components/profile/Profile"
+import Profile from "../components/Profile/Profile"
 
 const ProfilePage = () => {
     const data = ActivitiesJson
+    const activitiesMap = data.activities.map((act) => {return (<Profile aProps={act}/>)})
     return (
-        <Fragment><Profile aProps={data.activities[0]}/></Fragment>
+        <Fragment>
+            {activitiesMap}
+        </Fragment>
     )
 }
 
