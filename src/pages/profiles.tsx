a28c7bfd9f1a8126386bf7ce8e7383120707740a
Manuel Oliveira <mbroliveira@gmail.com>
2021-12-05 01:38:32 +0000

    version error

--- src/pages/profiles.tsx
+++ src/pages/profiles.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import ActivitiesJson from "../json/ActivitiesJson.json"
-import Profile from "../components/Profile/Profile"
+import Profile from "../components/profile/Profile"
 
 const ProfilePage = () => {
     const data = ActivitiesJson
