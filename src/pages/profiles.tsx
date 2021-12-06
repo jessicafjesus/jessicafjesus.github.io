280092d83f59d45313485ce6df37b08298b2dd7c
1000roy <a.milroy@campus.fct.unl.pt>
2021-12-06 19:07:35 +0000

    perfil-update

--- src/pages/profiles.tsx
+++ src/pages/profiles.tsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import MuseumsJson from "../json/MuseumsJson.json"
+import ProfilesJson from "../json/ProfilesJson.json"
 import Profile from "../components/profile/Profile"
 
 
 const ProfilePage = () => {
     const data = MuseumsJson
-    const museumsMap = data.museums.map((act) => {return (<Profile mProps={act}/>)})
+    const pData = ProfilesJson
+    const museumsMap = data.museums.map((act) => {return (<Profile museumP={act} profileP={pData.profiles[0]}/>)})
     return (
         <Fragment>
             {museumsMap}
